49061f23a72ee3ca1d826329e258c741f9294887
Josh Rehman <josh@joshrehman.com>
2013-06-06 14:41:02 -0700

    Rename the java verticle.

---
+++ src/main/java/com/javajosh/hyena/JavaVerticle.java
@@ -0,0 +1,19 @@
+package com.javajosh.hyena;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.platform.Verticle;
+
+public class JavaVerticle extends Verticle {
+
+    public void start() {
+        vertx.eventBus().registerHandler("ping-address", new Handler<Message<String>>() {
+            @Override
+            public void handle(Message<String> message) {
+                message.reply("pong!");
+                container.logger().info("Sent back pong");
+            }
+        });
+        container.logger().info("PingVerticle started");
+    }
+}
