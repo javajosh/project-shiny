62b40fd33a66782f38c92ef87c183dbf8d977f73
Josh Rehman <josh@joshrehman.com>
2013-06-11 15:50:10 -0700

    Having problems with recordparser, so simplify to a simpler data handler; this demonstrates the logging probem with gradle runmod

--- src/main/groovy/com/javajosh/shiny/GroovyVerticle.groovy
+++ src/main/groovy/com/javajosh/shiny/GroovyVerticle.groovy
@@ -10,11 +10,15 @@ import org.vertx.groovy.platform.Verticle
 class GroovyVerticle extends Verticle{
   Object start(){
     vertx.createNetServer().connectHandler {NetSocket socket ->
-      RecordParser parser = RecordParser.newDelimited("\n"){String payload ->
-        log.trace("$payload")
+//      RecordParser parser = RecordParser.newDelimited("\n"){String payload ->
+//        log.info("Verticle recieved $payload")
+//        println "Verticle recieved $payload"
+//      }
+      socket.dataHandler{data ->
+        log.info("$data")
+        println "$data"
       }
-      socket.dataHandler (parser.toClosure())
     }.listen(12345)
-    log.info("Start complete")
+    log.info("Start complete; listening on port 12345")
   }
 }
