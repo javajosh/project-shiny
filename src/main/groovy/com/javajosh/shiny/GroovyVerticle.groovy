0af48b2204fea739f47ea056253b8e4d8b9638db
Josh Rehman <josh@joshrehman.com>
2013-06-11 14:38:58 -0700

    instrument the verticle with logback logging using fancy groovy annotations. note that logback uses slf4j in the background. the much nicer named "@Log" uses java.util.logging.

--- src/main/groovy/com/javajosh/shiny/GroovyVerticle.groovy
+++ src/main/groovy/com/javajosh/shiny/GroovyVerticle.groovy
@@ -1,18 +1,21 @@
-package com.javajosh.hyena
+package com.javajosh.shiny
 
-import org.vertx.groovy.core.buffer.Buffer
+import groovy.util.logging.Slf4j
 import org.vertx.groovy.core.net.NetSocket
 import org.vertx.groovy.core.parsetools.RecordParser
 import org.vertx.groovy.platform.Verticle
 
-
+//see http://www.canoo.com/blog/2010/09/20/log-groovys-new-and-extensible-logging-conveniences/
+@Slf4j
 class GroovyVerticle extends Verticle{
   Object start(){
     vertx.createNetServer().connectHandler {NetSocket socket ->
       RecordParser parser = RecordParser.newDelimited("\n"){String payload ->
         //println "$payload"
+        log.trace("Hello Logger")
       }
       socket.dataHandler (parser.toClosure())
     }.listen(12345)
+    log.info("Start complete")
   }
 }
