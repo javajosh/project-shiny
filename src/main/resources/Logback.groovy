f835956a1021c7cba3e761034f60f6fadd2d2cd3
Josh Rehman <josh@joshrehman.com>
2013-06-11 15:16:49 -0700

    rename Logback.groovy to logback.groovy; add a file appender.

--- src/main/resources/Logback.groovy
+++ src/main/resources/Logback.groovy
@@ -1,5 +1,7 @@
+//This file defines mod-shiny's logging behavior. Right now, it poops stuff out to the console and to a file called log/mod-shiny.log relative to the project root.
 
-//imported by default:
+//see http://logback.qos.ch/manual/groovy.html for docs on this file's format
+//all this stuff gets imported by default:
 //import ch.qos.logback.core.*;
 //import ch.qos.logback.core.encoder.*;
 //import ch.qos.logback.core.read.*;
@@ -8,12 +10,19 @@
 //import ch.qos.logback.classic.net.*;
 //import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 
-//see http://logback.qos.ch/manual/groovy.html
 
-appender("STDOUT", ConsoleAppender) {
-  layout(PatternLayout) {
+appender("CONSOLE", ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
     pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
   }
 }
-  logger("com.javajosh", TRACE)
-root(DEBUG, ["STDOUT"])
+
+appender("FILE", FileAppender) {
+  file = "log/mod-shiny.log"
+  append = true
+  encoder(PatternLayoutEncoder) {
+    pattern = "%level %logger - %msg%n"
+  }
+}
+
+root(DEBUG, ["FILE", "CONSOLE"])
