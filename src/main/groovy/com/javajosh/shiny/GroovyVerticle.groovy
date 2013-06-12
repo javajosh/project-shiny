90aebdb9eaa09015d94a43260fc2956c983c2a9f
Josh Rehman <josh@joshrehman.com>
2013-06-12 08:35:31 -0700

    reduce the visability of record parser even further; add some logging.

--- src/main/groovy/com/javajosh/shiny/GroovyVerticle.groovy
+++ src/main/groovy/com/javajosh/shiny/GroovyVerticle.groovy
@@ -9,13 +9,17 @@ import org.vertx.groovy.platform.Verticle
 //see http://www.canoo.com/blog/2010/09/20/log-groovys-new-and-extensible-logging-conveniences/
 @Slf4j
 class GroovyVerticle extends Verticle{
-  private RecordParser parser = RecordParser.newDelimited("\n"){Buffer buffer ->
-    String payload = buffer.toString().trim()
-    log.info("RecordParser recieved $payload")
-  }
+  public static int PORT = 12345
 
+  @Override
   Object start(){
+    RecordParser parser = RecordParser.newDelimited("\n"){Buffer buffer ->
+      String payload = buffer.toString().trim()
+      log.info("GroovyVerticle recieved delimited $payload")
+    }
+
     vertx.createNetServer().connectHandler {NetSocket socket ->
+      log.info("socket connection ${socket.writeHandlerID}")
       socket.dataHandler (parser.toClosure())
       socket.closeHandler {
         log.info("socket closed")
