68ecde2197ca41733255b9b2a79c1503140ccfd8
Josh Rehman <josh@joshrehman.com>
2013-06-12 08:35:02 -0700

    break up the tests a bit more to simulate what will happen in real life.

--- src/test/groovy/LogicTestGroovy.groovy
+++ src/test/groovy/LogicTestGroovy.groovy
@@ -3,13 +3,19 @@ import org.junit.Test
 
 import static org.junit.Assert.assertTrue
 
-
 public class LogicUnitTest {
-
   @Test
   public void testAnd() {
     assertTrue(Logic.and(true, true))
+  }
+
+  @Test
+  public void testNot() {
     assertTrue(Logic.not(false))
+  }
+
+  @Test
+  public void testOr() {
     assertTrue(Logic.or(true, false))
   }
 }
